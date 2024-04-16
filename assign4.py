13c7bd84d6be9b706ec0034add66f9fd63643c7c
sanskrutimhatre21 <mhatresanskruti21@gmail.com>
2024-04-16 23:12:35 +0530

    assign4 modified

--- assign4.py
+++ assign4.py
@@ -4,8 +4,14 @@ num2 = int(input("Enter num2 : "))
 num3 = int(input("Enter num3 : "))
 
 
-if num1 > num2:
-        print("num1 is greater")
-elif num1 < num2:
-        print("num2 is greater")    
-else num1
+def maximum():
+
+if(num1 >= num2)and(num1 >= num3):
+                print("num1 is greater")
+elif(num2 >= num1)and(num2 >= num3):
+                print("num2 is greater")
+else:
+                print("num3 is greater")
+
+print(maximum)
+
